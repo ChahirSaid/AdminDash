900ec389a3d8a404b9717fe3e68bd03a60437111
Said Chahir <chahirsaid18@gmail.com>
2024-03-29 16:17:38 +0000

    AreaTableAction fix

--- React-app/src/components/dashboard/areaTable/AreaTableAction.jsx
+++ React-app/src/components/dashboard/areaTable/AreaTableAction.jsx
@@ -1,40 +1,18 @@
-import { useState } from "react";
-import { HiDotsHorizontal } from "react-icons/hi";
+import { BsTrash } from "react-icons/bs";
 
-const AreaTableAction = ({ orderId, handleDelete, closeDropdown }) => {
-  const [showDropdown, setShowDropdown] = useState(false);
-
-  const handleDropdown = () => {
-    setShowDropdown(!showDropdown);
-    closeDropdown();
-  };
-
+const AreaTableAction = ({ orderId, handleDelete }) => {
   const handleClickDelete = () => {
     handleDelete(orderId);
-    setShowDropdown(false);
   };
 
   return (
-    <>
-      <button
-        type="button"
-        className="action-dropdown-btn"
-        onClick={handleDropdown}
-      >
-        <HiDotsHorizontal size={18} />
-        {showDropdown && (
-          <div className="action-dropdown-menu">
-            <ul className="dropdown-menu-list">
-              <li className="dropdown-menu-item">
-                <button className="dropdown-menu-link" onClick={handleClickDelete}>
-                  Delete
-                </button>
-              </li>
-            </ul>
-          </div>
-        )}
-      </button>
-    </>
+    <button
+      type="button"
+      className="action-dropdown-btn"
+      onClick={handleClickDelete}
+    >
+      <BsTrash size={18} />
+    </button>
   );
 };
 
